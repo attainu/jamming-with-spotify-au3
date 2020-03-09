788c5f4ef8195538e20ea3b6cb225158ef0897ff
Aswathy Raj <asraj@Aruns-MacBook-Air.local>
2020-03-09 22:02:05 +0530

    Heroku changes

--- server/authorization_code/app.js
+++ server/authorization_code/app.js
@@ -152,7 +152,7 @@ app.get("/callback", function(req, res) {
 
         // we can also pass the token to the browser to make requests from there
         res.redirect(
-          "/app:" +
+          "/app/:" +
             querystring.stringify({
               access_token: access_token,
               refresh_token: refresh_token
@@ -170,7 +170,7 @@ app.get("/callback", function(req, res) {
   }
 });
 
-app.get("/app*", function(req, res) {
+app.get("/app/*", function(req, res) {
   console.log(__dirname);
   //res.sendFile("./index.html");
   res.sendFile(path.join(__dirname, "../../client/build/index.html"));
