ba1e9533511f8803e12e96de1413919b763aed92
Aswathy Raj <asraj@Aruns-MacBook-Air.local>
2020-03-09 17:06:27 +0530

    Heroku changes

--- server/authorization_code/app.js
+++ server/authorization_code/app.js
@@ -127,7 +127,7 @@ app.get("/callback", function(req, res) {
 
         // we can also pass the token to the browser to make requests from there
         res.redirect(
-          "http://localhost:3000/:" +
+          "https://jamming-spotify.herokuapp.com/:" +
             querystring.stringify({
               access_token: access_token,
               refresh_token: refresh_token
