5d3e64cc2ccec0b2253c9766ecb8b4e85d230093
Aswathy Raj <asraj@Aruns-MacBook-Air.local>
2020-03-09 22:17:49 +0530

    Heroku changes

--- client/src/components/Logout/index.js
+++ client/src/components/Logout/index.js
@@ -11,7 +11,7 @@ const Logout = () => {
     );
     setTimeout(() => {
       spotifyLogoutWindow.close();
-      window.location.href = "https://jamming-spotify.herokuapp.com/login";
+      window.location.href = "https://jamming-spotify.herokuapp.com/";
     }, 2000);
   }
 
