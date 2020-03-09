0dcbdaa632c20a96bfb973e06a6532a15ec69f11
Aswathy Raj <asraj@Aruns-MacBook-Air.local>
2020-03-09 20:16:49 +0530

    Heroku changes

--- server/authorization_code/app.js
+++ server/authorization_code/app.js
@@ -173,7 +173,7 @@ app.get("/callback", function(req, res) {
 
 app.get("/app", function(req, res) {
   console.log(__dirname);
-  res.sendFile(path.join(__dirname + "../../client/build/index.html"));
+  res.sendFile(path.join(__dirname + "../../client/build/public/index.html"));
 });
 
 app.get("/refresh_token", function(req, res) {
