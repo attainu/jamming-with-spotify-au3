3581e6c98ee3ba89b67705833a097e1898e8670d
Aswathy Raj <asraj@Aruns-MacBook-Air.local>
2020-03-09 22:38:36 +0530

    Heroku changes

--- server/authorization_code/app.js
+++ server/authorization_code/app.js
@@ -77,27 +77,27 @@ app.get("/login", function(req, res) {
   // your application requests authorization
   var scope =
     "user-top-read user-read-recently-played playlist-read-private playlist-read-collaborative  playlist-modify-public playlist-modify-private ugc-image-upload user-follow-modify user-follow-read user-library-read user-library-modify user-read-private user-read-email user-top-read user-read-playback-state";
-  // res.redirect(
-  //   "https://accounts.spotify.com/authorize?" +
-  //     querystring.stringify({
-  //       response_type: "code",
-  //       client_id: client_id,
-  //       scope: scope,
-  //       redirect_uri: redirect_uri,
-  //       state: state
-  //     })
-  // );
   res.redirect(
-    "https://accounts.spotify.com/authorize" +
-      "?response_type=code" +
-      "&client_id=" +
-      client_id +
-      (scope ? "&scope=" + encodeURIComponent(scope) : "") +
-      "&redirect_uri=" +
-      encodeURIComponent(redirect_uri) +
-      "&state=" +
-      state
+    "https://accounts.spotify.com/authorize?" +
+      querystring.stringify({
+        response_type: "code",
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state
+      })
   );
+  // res.redirect(
+  //   "https://accounts.spotify.com/authorize" +
+  //     "?response_type=code" +
+  //     "&client_id=" +
+  //     client_id +
+  //     (scope ? "&scope=" + encodeURIComponent(scope) : "") +
+  //     "&redirect_uri=" +
+  //     encodeURIComponent(redirect_uri) +
+  //     "&state=" +
+  //     state
+  // );
 });
 
 app.get("/callback", function(req, res) {
